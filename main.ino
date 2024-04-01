d14bda04f8eb2c9d3d4f55305daa594ee379cec0
esf4613 <78970579+esf4613@users.noreply.github.com>
2024-04-01 14:15:37 -0400

    Add files via upload

--- main.ino
+++ main.ino
@@ -4,8 +4,8 @@
 Servo servo;
 const byte servoPin = A3; // coolant enable
 byte pos = 0;
-const int servoClosed = 5;
-const int servoOpen = 60;
+const int servoClosed = 15; // Servo angle closed
+const int servoOpen = 100; // Servo angle open
 
 // Stepper variables
 const int enPin=8;
@@ -16,9 +16,9 @@ const int dirYPin = 6; // Y.DIR
 const int stepZPin = 4; //Z.STEP
 const int dirZPin = 7; // Z.DIR
 
-const int h_steps=1000; // Horizontal stepper steps (200 per revolution)
-const int v_steps=1000; // Horizontal stepper steps (200 per revolution)
-const int c_steps=105; // Horizontal stepper steps (200 per revolution)
+const int x_steps=7300; // Horizontal stepper steps (200 per revolution)
+const int y_steps=10500; // Horizontal stepper steps (200 per revolution) [10,500 is highest value (max depth)]
+const int z_steps=105; // Horizontal stepper steps (200 per revolution)
 int pulseWidthMicros = 100; 	// microseconds
 int millisBtwnSteps = 1000;
 
@@ -44,21 +44,31 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(F("Motor Control Menu:\n1 - Horizontal\n2 - Vertical\n3 - Claw\n4 - Collection Cylinder\nChoice: "));
+  Serial.println(F("Motor Control Menu:\n0 - Demo Program\n1 - Horizontal\n2 - Vertical\n3 - Claw\n4 - Collection Cylinder\nChoice: "));
   char motor = Serial.parseInt();
   int direction;
-
-  if(motor == 1){ // Horizontal Motor
+  
+  if(motor == 0){
+    Serial.println(F("Demo Program selected."));
+    servo.write(servoOpen); // Open the claw
+    h_v_abduction(); // Move claw over and down to the soil
+    servo.write(servoClosed); // Close the claw
+    h_v_adduction(); // Move claw back and up to the collection funnel
+    servo.write(servoOpen); // Deposit the sample
+    z_clockwise(); // Rotate to next sample
+    servo.write(servoClosed); // Close the claw
+  }
+  else if(motor == 1){ // Horizontal Motor
     Serial.println(F("Horizontal Motor selected."));
     Serial.println(F("Choose Direction:\n1 - Forward\n2 - Backward\nChoice: "));
     direction = Serial.parseInt();
     if (direction == 1){
       Serial.println(F("Forward selected. Driving..."));
-      h_forward();
+      x_forward();
     }
     else if (direction == 2){
       Serial.println(F("Backward selected. Driving..."));
-      h_backward();
+      x_backward();
     }
     else{
       Serial.println(F("Incorrect option entered. Please try again."));
@@ -70,11 +80,11 @@ void loop() {
     direction = Serial.parseInt();
     if (direction == 1){
       Serial.println(F("Downward selected. Driving..."));
-      v_forward();
+      y_forward();
     }
     else if (direction == 2){
       Serial.println(F("Upward selected. Driving..."));
-      v_backward();
+      y_backward();
     }
     else{
       Serial.println(F("Incorrect option entered. Please try again.\n"));
@@ -102,11 +112,11 @@ void loop() {
     direction = Serial.parseInt();
     if (direction == 1){
       Serial.println(F("Clockwise selected. Driving..."));
-      c_clockwise();
+      z_clockwise();
     }
     else if (direction == 2){
       Serial.println(F("Counterclockwise selected. Driving..."));
-      c_counterclockwise();
+      z_counterclockwise();
     }
     else{
       Serial.println(F("Incorrect option entered. Please try again.\n"));
@@ -114,11 +124,11 @@ void loop() {
   }
 }
 
-void h_forward(){
-  Serial.println(F("Running clockwise"));
+void x_forward(){
+  Serial.println(F("Running clockwise..."));
  	digitalWrite(dirXPin, HIGH); // Enables the motor to move in a particular direction
  	// Makes 200 pulses for making one full cycle rotation
- 	for (int i = 0; i < h_steps; i++) {
+ 	for (int i = 0; i < x_steps; i++) {
  			digitalWrite(stepXPin, HIGH);
  			delayMicroseconds(pulseWidthMicros);
  			digitalWrite(stepXPin, LOW);
@@ -126,11 +136,11 @@ void h_forward(){
  	}
 }
 
-void h_backward(){
-  Serial.println(F("Running counter-clockwise"));
+void x_backward(){
+  Serial.println(F("Running counter-clockwise..."));
  	digitalWrite(dirXPin, LOW); //Changes the rotations direction
  	// Makes 400 pulses for making two full cycle rotation
- 	for (int i = 0; i < h_steps; i++) {
+ 	for (int i = 0; i < x_steps; i++) {
  			digitalWrite(stepXPin, HIGH);
  			delayMicroseconds(pulseWidthMicros);
  			digitalWrite(stepXPin, LOW);
@@ -138,11 +148,11 @@ void h_backward(){
  	}
 }
 
-void v_forward(){
-  Serial.println(F("Running clockwise"));
+void y_forward(){
+  Serial.println(F("Running clockwise..."));
  	digitalWrite(dirYPin, HIGH); // Enables the motor to move in a particular direction
  	// Makes 200 pulses for making one full cycle rotation
- 	for (int i = 0; i < v_steps; i++) {
+ 	for (int i = 0; i < y_steps; i++) {
  			digitalWrite(stepYPin, HIGH);
  			delayMicroseconds(pulseWidthMicros);
  			digitalWrite(stepYPin, LOW);
@@ -150,11 +160,11 @@ void v_forward(){
  	}
 }
 
-void v_backward(){
-  Serial.println(F("Running counter-clockwise"));
+void y_backward(){
+  Serial.println(F("Running counter-clockwise..."));
  	digitalWrite(dirYPin, LOW); //Changes the rotations direction
  	// Makes 400 pulses for making two full cycle rotation
- 	for (int i = 0; i < v_steps; i++) {
+ 	for (int i = 0; i < y_steps; i++) {
  			digitalWrite(stepYPin, HIGH);
  			delayMicroseconds(pulseWidthMicros);
  			digitalWrite(stepYPin, LOW);
@@ -162,11 +172,11 @@ void v_backward(){
  	}
 }
 
-void c_clockwise(){
-  Serial.println(F("Running clockwise"));
+void z_clockwise(){
+  Serial.println(F("Running clockwise..."));
   digitalWrite(dirZPin, LOW); //Changes the rotations direction
  	// Makes 400 pulses for making two full cycle rotation
- 	for (int i = 0; i < c_steps; i++) {
+ 	for (int i = 0; i < z_steps; i++) {
  			digitalWrite(stepZPin, HIGH);
  			delayMicroseconds(pulseWidthMicros);
  			digitalWrite(stepZPin, LOW);
@@ -174,14 +184,56 @@ void c_clockwise(){
  	}
 }
 
-void c_counterclockwise(){
-  Serial.println(F("Running counter-clockwise"));
+void z_counterclockwise(){
+  Serial.println(F("Running counter-clockwise..."));
  	digitalWrite(dirZPin, HIGH); // Enables the motor to move in a particular direction
  	// Makes 200 pulses for making one full cycle rotation
- 	for (int i = 0; i < c_steps; i++) {
+ 	for (int i = 0; i < z_steps; i++) {
  			digitalWrite(stepZPin, HIGH);
  			delayMicroseconds(pulseWidthMicros);
  			digitalWrite(stepZPin, LOW);
+ 			delayMicroseconds(millisBtwnSteps);
+ 	}
+}
+
+void h_v_abduction(){
+  Serial.println(F("Running horizontal and vertical motors in abduction..."));
+  digitalWrite(dirXPin, HIGH);
+  digitalWrite(dirYPin, HIGH);
+
+  // Move the horizotal and vertical motors together to pick up a sample
+ 	for (int i = 0; i < y_steps; i++) {
+      if(i < x_steps){
+ 			  digitalWrite(stepXPin, HIGH); // Horizontal motor steps will stop first
+      }
+      digitalWrite(stepYPin, HIGH);
+ 			delayMicroseconds(pulseWidthMicros);
+
+      if(i < x_steps){
+ 			  digitalWrite(stepXPin, LOW); // Horizontal motor steps will stop first
+      }
+      digitalWrite(stepYPin, LOW);
+ 			delayMicroseconds(millisBtwnSteps);
+ 	}
+}
+
+void h_v_adduction(){
+  Serial.println(F("Running horizontal and vertical motors in adduction..."));
+  digitalWrite(dirXPin, LOW);
+  digitalWrite(dirYPin, LOW);
+
+  // Move the horizotal and vertical motors together to retrieve a sample
+ 	for (int i = 0; i < y_steps; i++) {
+      if(i < x_steps){
+ 			  digitalWrite(stepXPin, HIGH); // Horizontal motor steps will stop first
+      }
+      digitalWrite(stepYPin, HIGH);
+ 			delayMicroseconds(pulseWidthMicros);
+      
+      if(i < x_steps){
+ 			  digitalWrite(stepXPin, LOW); // Horizontal motor steps will stop first
+      }
+      digitalWrite(stepYPin, LOW);
  			delayMicroseconds(millisBtwnSteps);
  	}
 }
