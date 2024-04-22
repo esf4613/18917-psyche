eabd1f6f8b4ef6492d6a96b9ecb42146df8e4552
esf4613 <78970579+esf4613@users.noreply.github.com>
2024-04-22 15:41:21 -0400

    Add files via upload

--- main.ino
+++ main.ino
@@ -1,11 +1,12 @@
 #include <Servo.h>
+#include <Adafruit_VCNL4040.h>
 
 // Servo variables
 Servo servo;
 const byte servoPin = A3; // coolant enable
 byte pos = 0;
-const int servoClosed = 15; // Servo angle closed
-const int servoOpen = 100; // Servo angle open
+const int servoClosed = 0; // Servo angle closed
+const int servoOpen = 90; // Servo angle open
 
 // Stepper variables
 const int enPin=8;
@@ -17,15 +18,22 @@ const int stepZPin = 4; //Z.STEP
 const int dirZPin = 7; // Z.DIR
 
 // SparkFun VCNL4040 Proximity Sensors
+Adafruit_VCNL4040 vcnl4040 = Adafruit_VCNL4040(); // Sensor object
 const int proxMin = 0; // Nothing is detected
 const int proxMax = 65535; // Max value for detecting an object
-const int proxThresh = 200; // Value for sensor proximity
+const int proxThresh = 2; // Value for sensor proximity
 
 const int x_steps=7300; // Horizontal stepper steps (200 per revolution)
-const int y_steps=10500; // Horizontal stepper steps (200 per revolution) [10,500 is highest value (max depth)]
-const int z_steps=105; // Horizontal stepper steps (200 per revolution)
+const int x_steps_to_funnel=1400; // The number of steps from the base of the horiztonal motor to funnel
+const int y_steps=9700; // Horizontal stepper steps (200 per revolution) [10,500 is highest value (max depth)]
+const int y_steps_to_funnel=2000; // The number of steps from the base of the vertical motor to funnel
+const int z_steps=108; // Horizontal stepper steps (200 per revolution)
+
+// Milliseconds between each step, higher value means a stronger drive, but slower
+int x_microsBtwnSteps = 1500;
+int y_microsBtwnSteps = 1000;
+int z_millisBtwnSteps = 3000;
 int pulseWidthMicros = 100; 	// microseconds
-int millisBtwnSteps = 1000;
 
 int motorMoveTime = 10000; // 10 Seconds
 
@@ -46,24 +54,17 @@ void setup() {
 
   // Servo setup
   servo.attach(servoPin);
+
+  // Proximity Sensor setup
+  vcnl4040.begin();
 }
 
 void loop() {
-  Serial.println(F("Motor Control Menu:\n0 - Demo Program\n1 - Horizontal\n2 - Vertical\n3 - Claw\n4 - Collection Cylinder\nChoice: "));
+  Serial.println(F("Motor Control Menu:\n1 - Horizontal\n2 - Vertical\n3 - Claw\n4 - Collection Cylinder\n5 - Demo Program\nChoice: "));
   char motor = Serial.parseInt();
   int direction;
   
-  if(motor == 0){
-    Serial.println(F("Demo Program selected."));
-    servo.write(servoOpen); // Open the claw
-    h_v_abduction(); // Move claw over and down to the soil
-    servo.write(servoClosed); // Close the claw
-    h_v_adduction(); // Move claw back and up to the collection funnel
-    servo.write(servoOpen); // Deposit the sample
-    z_clockwise(); // Rotate to next sample
-    servo.write(servoClosed); // Close the claw
-  }
-  else if(motor == 1){ // Horizontal Motor
+  if(motor == 1){ // Horizontal Motor
     Serial.println(F("Horizontal Motor selected."));
     Serial.println(F("Choose Direction:\n1 - Forward\n2 - Backward\nChoice: "));
     direction = Serial.parseInt();
@@ -127,6 +128,19 @@ void loop() {
       Serial.println(F("Incorrect option entered. Please try again.\n"));
     }
   }
+  else if(motor == 5){
+    Serial.println(F("Demo Program selected."));
+    servo.write(servoOpen); // Open the claw
+    h_v_abduction(); // Move claw over and down to the soil
+    servo.write(servoClosed); // Close the claw
+    delay(1000); // Wait 1 second while collecting sample
+    h_v_adduction(); // Move claw back and up to the collection funnel
+    servo.write(servoOpen); // Deposit the sample
+    delay(1000); // Wait 1 second while depositing sample
+    xy_zero_out(); // Return x and y steppers to base (zeroed-out)
+    z_clockwise(); // Rotate to next sample
+    servo.write(servoClosed); // Close the claw
+  }
 }
 
 void x_forward(){
@@ -137,7 +151,7 @@ void x_forward(){
  			digitalWrite(stepXPin, HIGH);
  			delayMicroseconds(pulseWidthMicros);
  			digitalWrite(stepXPin, LOW);
- 			delayMicroseconds(millisBtwnSteps);
+ 			delayMicroseconds(x_microsBtwnSteps);
  	}
 }
 
@@ -149,7 +163,19 @@ void x_backward(){
  			digitalWrite(stepXPin, HIGH);
  			delayMicroseconds(pulseWidthMicros);
  			digitalWrite(stepXPin, LOW);
- 			delayMicroseconds(millisBtwnSteps);
+ 			delayMicroseconds(x_microsBtwnSteps);
+ 	}
+}
+
+void x_backward_steps(int steps){
+  Serial.println(F("Running counter-clockwise..."));
+ 	digitalWrite(dirXPin, LOW); //Changes the rotations direction
+ 	// Makes 400 pulses for making two full cycle rotation
+ 	for (int i = 0; i < steps; i++) {
+ 			digitalWrite(stepXPin, HIGH);
+ 			delayMicroseconds(pulseWidthMicros);
+ 			digitalWrite(stepXPin, LOW);
+ 			delayMicroseconds(x_microsBtwnSteps);
  	}
 }
 
@@ -161,19 +187,29 @@ void y_forward(){
  			digitalWrite(stepYPin, HIGH);
  			delayMicroseconds(pulseWidthMicros);
  			digitalWrite(stepYPin, LOW);
- 			delayMicroseconds(millisBtwnSteps);
+ 			delayMicroseconds(y_microsBtwnSteps);
  	}
 }
 
 void y_backward(){
-  Serial.println(F("Running counter-clockwise..."));
  	digitalWrite(dirYPin, LOW); //Changes the rotations direction
  	// Makes 400 pulses for making two full cycle rotation
  	for (int i = 0; i < y_steps; i++) {
  			digitalWrite(stepYPin, HIGH);
  			delayMicroseconds(pulseWidthMicros);
  			digitalWrite(stepYPin, LOW);
- 			delayMicroseconds(millisBtwnSteps);
+ 			delayMicroseconds(y_microsBtwnSteps);
+ 	}
+}
+
+void y_backward_steps(int steps){
+ 	digitalWrite(dirYPin, LOW); //Changes the rotations direction
+ 	// Makes 400 pulses for making two full cycle rotation
+ 	for (int i = 0; i < steps; i++) {
+ 			digitalWrite(stepYPin, HIGH);
+ 			delayMicroseconds(pulseWidthMicros);
+ 			digitalWrite(stepYPin, LOW);
+ 			delayMicroseconds(y_microsBtwnSteps);
  	}
 }
 
@@ -185,7 +221,7 @@ void z_clockwise(){
  			digitalWrite(stepZPin, HIGH);
  			delayMicroseconds(pulseWidthMicros);
  			digitalWrite(stepZPin, LOW);
- 			delayMicroseconds(millisBtwnSteps);
+ 			delayMicroseconds(z_millisBtwnSteps);
  	}
 }
 
@@ -197,10 +233,11 @@ void z_counterclockwise(){
  			digitalWrite(stepZPin, HIGH);
  			delayMicroseconds(pulseWidthMicros);
  			digitalWrite(stepZPin, LOW);
- 			delayMicroseconds(millisBtwnSteps);
+ 			delayMicroseconds(z_millisBtwnSteps);
  	}
 }
 
+// Moving downward
 void h_v_abduction(){
   Serial.println(F("Running horizontal and vertical motors in abduction..."));
   digitalWrite(dirXPin, HIGH);
@@ -208,24 +245,38 @@ void h_v_abduction(){
 
   // Move the horizotal and vertical motors together to pick up a sample
  	for (int i = 0; i < y_steps; i++) {
-      unsigned int proxValue = proximitySensor.getProximity(); // VCNL4040 Proximity Sensor
-      if(proxValue > proxThresh) {
+    /*
+    // Read the proximity value every 500 ms
+    if (i*(int)((double)x_microsBtwnSteps/1000.0) % 500 == 0){
+    Serial.print("Proximity:"); Serial.println(vcnl4040.getProximity());
+    }
+    
+    if (i > y_steps-2000){
+      unsigned int proxValue = vcnl4040.getProximity(); // VCNL4040 Proximity Sensor
+      Serial.print("Proximity:"); Serial.println(vcnl4040.getProximity());
+      if(proxValue >= proxThresh && proxValue < proxMax) {
+        Serial.println("Proximity value: ");
+        Serial.println(proxValue);
         break;
       }
-      if(i < x_steps){
- 			  digitalWrite(stepXPin, HIGH); // Horizontal motor steps will stop first
-      }
-      digitalWrite(stepYPin, HIGH);
- 			delayMicroseconds(pulseWidthMicros);
+    }
+    */
 
-      if(i < x_steps){
- 			  digitalWrite(stepXPin, LOW); // Horizontal motor steps will stop first
-      }
-      digitalWrite(stepYPin, LOW);
- 			delayMicroseconds(millisBtwnSteps);
+    if(i < x_steps){
+      digitalWrite(stepXPin, HIGH); // Horizontal motor steps will stop first
+    }
+    digitalWrite(stepYPin, HIGH);
+    delayMicroseconds(pulseWidthMicros);
+
+    if(i < x_steps){
+      digitalWrite(stepXPin, LOW); // Horizontal motor steps will stop first
+    }
+    digitalWrite(stepYPin, LOW);
+    delayMicroseconds(x_microsBtwnSteps);
  	}
 }
 
+// Moving upward
 void h_v_adduction(){
   Serial.println(F("Running horizontal and vertical motors in adduction..."));
   digitalWrite(dirXPin, LOW);
@@ -233,20 +284,38 @@ void h_v_adduction(){
 
   // Move the horizotal and vertical motors together to retrieve a sample
  	for (int i = 0; i < y_steps; i++) {
-      unsigned int proxValue = proximitySensor.getProximity(); // VCNL4040 Proximity Sensor
-      if(proxValue > proxThresh) {
-        break;
+      if(i < ((x_steps+3200)-x_steps_to_funnel) && i >= 3200){ // Need vertical to go up first to avoid hitting claw on protrusion
+ 			  digitalWrite(stepXPin, HIGH); // Horizontal motor steps will stop first
       }
-      if(i < x_steps){
+      if(i < (y_steps-y_steps_to_funnel)){ // Need vertical to stop so sample can be deposited close to funnel
+        digitalWrite(stepYPin, HIGH);
+      }
+ 			delayMicroseconds(pulseWidthMicros);
+      
+      if(i < ((x_steps+3200)-x_steps_to_funnel) && i >= 3200){
+ 			  digitalWrite(stepXPin, LOW); // Horizontal motor steps will stop first
+      }
+      if(i < (y_steps-y_steps_to_funnel)){
+        digitalWrite(stepYPin, LOW);
+      }
+ 			delayMicroseconds(x_microsBtwnSteps);
+ 	}
+}
+
+/* Used only in the demo program, assumes that x and y steppers are positioned over funnel */
+void xy_zero_out(){
+  // Move the horizotal and vertical motors back to zeroed position
+ 	for (int i = 0; i < y_steps_to_funnel; i++) {
+      if(i < x_steps_to_funnel){
  			  digitalWrite(stepXPin, HIGH); // Horizontal motor steps will stop first
       }
       digitalWrite(stepYPin, HIGH);
  			delayMicroseconds(pulseWidthMicros);
       
-      if(i < x_steps){
+      if(i < x_steps_to_funnel){
  			  digitalWrite(stepXPin, LOW); // Horizontal motor steps will stop first
       }
       digitalWrite(stepYPin, LOW);
- 			delayMicroseconds(millisBtwnSteps);
+ 			delayMicroseconds(x_microsBtwnSteps);
  	}
 }
